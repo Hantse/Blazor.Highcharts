e6a80b8ced60db90dd95ee39dcda36e1db141ab0
Patrick Grasseels <grasseelsp@hotmail.com>
2018-12-14 17:49:31 +0100

    Update ignore file

--- src/Blazor.Highcharts/content/blazorHighcharts.ts
+++ src/Blazor.Highcharts/content/blazorHighcharts.ts
@@ -1,6 +1,6 @@
 ﻿declare var Highcharts;
 const highCharts =
 {
-    init: (chartModel) => Highcharts.chart('chartContainerHighCharts', chartModel)
+    init: (chartModel: any) => Highcharts.chart('chartContainerHighCharts', chartModel)
 };
 window['highCharts'] = highCharts;
