8ea7f7e55fcd92436143a0e86d2bbd7881ffde64
Patrick Grasseels <grasseelsp@hotmail.com>
2018-12-14 17:50:45 +0100

    Remove file

--- src/Blazor.Highcharts.Sample/bin/Debug/netstandard2.0/dist/_content/Blazor.Highcharts/blazorHighcharts.js
+++
@@ -1,5 +0,0 @@
-var highCharts = {
-    init: function (chartModel) { return Highcharts.chart('chartContainerHighCharts', chartModel); }
-};
-window['highCharts'] = highCharts;
-//# sourceMappingURL=blazorHighcharts.js.map
--- src/Blazor.Highcharts.Sample/bin/Debug/netstandard2.0/dist/_content/IceArtefact.Core.Blazor.Highcharts/iceArtefactHighcharts.ts
+++
@@ -1,6 +0,0 @@
-﻿declare var Highcharts;
-const highCharts =
-{
-    init: (chartModel) => Highcharts.chart('chartContainerHighCharts', chartModel)
-};
-window['highCharts'] = highCharts;
